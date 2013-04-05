63572ad9a1d5fbdd97fe94451855b25be20dd11a
Vincent Hanquez <vincent@snarc.org>
2013-04-05 08:42:03 +0100

    add a UniqueId reader from string and read instance. not very efficient but get the job done.

--- Data/Vhd/Types.hs
+++ Data/Vhd/Types.hs
@@ -2,6 +2,7 @@ module Data.Vhd.Types where
 
 import Control.Exception
 import Control.Monad
+import Data.Char
 import qualified Data.ByteString as B
 import Data.List
 import qualified Data.Text as T
@@ -99,6 +100,28 @@ newtype UniqueId             = UniqueId           B.ByteString deriving (Eq)
 instance Show UniqueId where
     show (UniqueId b) = intercalate "-" $ map disp [[0 .. 3], [4, 5], [6, 7], [8, 9], [10 .. 15]]
         where disp = concatMap (printf "%02x" . B.index b)
+
+ofString :: String -> Maybe UniqueId
+ofString s
+    | length s /= 36 = Nothing
+    | head r0 /= '-' = Nothing
+    | head r1 /= '-' = Nothing
+    | head r2 /= '-' = Nothing
+    | head r3 /= '-' = Nothing
+    | otherwise      = Just $ UniqueId $ B.pack (unhex a8 ++ unhex b4 ++ unhex c4 ++ unhex d4 ++ unhex (drop 1 r3))
+  where (a8, r0) = splitAt 8 s
+        (b4, r1) = splitAt 4 $ drop 1 r0
+        (c4, r2) = splitAt 4 $ drop 1 r1
+        (d4, r3) = splitAt 4 $ drop 1 r2
+
+        unhex []         = []
+        unhex (v1:v2:xs) = fromIntegral (digitToInt v1 * 16 + digitToInt v2) : unhex xs
+
+instance Read UniqueId where
+    readsPrec _ r = let (t,d) = splitAt 36 r
+                     in case ofString t of
+                            Nothing  -> []
+                            Just uid -> [(uid, d)]
 
 newtype ParentLocatorEntries = ParentLocatorEntries [ParentLocatorEntry] deriving (Show, Eq)
 
