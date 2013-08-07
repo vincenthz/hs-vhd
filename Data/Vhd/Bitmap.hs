3298c9d7750edc887acebcb5aa4b9e29cda3749a
Vincent Hanquez <vincent@snarc.org>
2013-08-07 07:05:25 +0100

    prevent re-defining bits from Data.Bits by using a different symbol

--- Data/Vhd/Bitmap.hs
+++ Data/Vhd/Bitmap.hs
@@ -17,14 +17,14 @@ bitmapGet :: Bitmap -> Int -> IO Bool
 bitmapGet (Bitmap ptr) n = test `fmap` peekByteOff ptr offset
     where
         test :: Word8 -> Bool
-        test = flip testBit (7 - bit)
-        (offset, bit) = n `divMod` 8
+        test = flip testBit (7 - nBit)
+        (offset, nBit) = n `divMod` 8
 
 bitmapModify :: Bitmap -> Int -> (Int -> Word8 -> Word8) -> IO ()
-bitmapModify (Bitmap bptr) n f = peek ptr >>= poke ptr . f (7 - bit)
+bitmapModify (Bitmap bptr) n f = peek ptr >>= poke ptr . f (7 - nBit)
     where
         ptr = bptr `plusPtr` offset
-        (offset, bit) = n `divMod` 8
+        (offset, nBit) = n `divMod` 8
 
 bitmapSet :: Bitmap -> Int -> IO ()
 bitmapSet bitmap n = bitmapModify bitmap n (flip setBit)
